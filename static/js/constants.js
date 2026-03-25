540a31fddbc35caa55f09cc89e46f72717b0bc40
liuk3 <70254810+liuk3@users.noreply.github.com>
2026-03-25 17:28:19 +0100

    aggiunto l'ultimo puzzle funzionante

--- static/js/constants.js
+++ static/js/constants.js
@@ -3,9 +3,9 @@
 const PUZZLE_URLS = [
     "https://puzzle-maker.online/crossword-1d83087-luogo-del-delitto?lang=it", // 1. Luogo
     "https://puzzle-maker.online/crossword-1d83369-tempo-del-delitto", // 2. Tempo
-    "https://puzzle-maker.online/crossword-1d7gf82-movente-del-delitto?lang=it", // 3. Movente
+    "https://puzzle-maker.online/crossword-1d83d64-arma-del-delitto", // 3. Arma
     "https://puzzle-maker.online/crossword-1d7gf82-movente-del-delitto?lang=it", // 4. Movente
-    "https://puzzle-maker.online/crossword-1d7gf82-assassino-del-delitto?lang=it"  // 5. Assassino (Pergamena)
+    "https://puzzle-maker.online/crossword-1d85176-colpevole"  // 5. Colpevole (Pergamena)
 ];
 
 const STEP_NAMES = [
